6b9253e5d36a7c9223667c6677c4d3899004efce
Alex Macdonald <almacdon@redhat.com>
2024-06-13 11:17:40 -0400

    Add custom perspective

---
+++ src/app/utils/perspective.ts
@@ -0,0 +1,7 @@
+import testIcon from '../test-icon';
+
+export const icon = { default: testIcon };
+
+export const getLandingPageURL = () => '/dashboard';
+
+export const getImportRedirectURL = (namespace: string) => `/dashboard`;
